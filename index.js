6d2ef1230474ebb6d25aafffb29ac67d4ec7e36f
Henry Ho <henrychho89@gmail.com>
2020-02-15 09:25:34 -0800

    Added process environmetn port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express()
 
 const FILE_NAME = "Artists.json"
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
