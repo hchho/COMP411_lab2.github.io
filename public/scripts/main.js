0f699a29df15b029935294cc11fa3b423ef8a8a2
Henry Ho <henrychho89@gmail.com>
2020-03-02 22:21:22 -0800

    Added delete functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,103 +7,15 @@ const showForm = () => {
     const formContainer = document.querySelector("div.form_container");
     const isFormVisible = formContainer.style.display === "flex";
     formContainer.style.display = isFormVisible ? "none" : "flex"; // reverse visibility
-}
+}   
 
-const loadArtists = () =>
-    fetch('/getAllArtists', {
-        method: 'GET',
-        // headers: DEFAULT_HEADER
-    })
-        // .then(() => console.log("load success"))
-        .then(res => res)
-        .catch(err => console.error(err))
-
-const initialLoad = () => loadArtists().then(data => document.write(data))
-
-const handleAddArtistClick = () => {
-    const artistForm = document.forms["artistForm"]
-    const artist = Object.assign({}, {
-        name: artistForm.artistName.value,
-        desc: artistForm.artistDesc.value,
-        img: artistForm.artistImg.value
-    })
-
-    const newId = (new Date()).getTime()
-
-    addArtistToFile(artist, newId)
-}
-
-const addArtistToFile = (artist, newId) => {
-    const rawBody = {}
-    rawBody[`${newId}`] = artist
-    fetch('/addNewArtist', {
-        method: 'POST',
-        headers: DEFAULT_HEADER,
-        body: JSON.stringify(rawBody)
-    })
-        // .then(() => console.log("Add success"))
-}
-
-const addArtistToDom = (obj, newId) => {
-    const { artistName, artistDesc, artistImg } = obj
-
-    if (artistName.length > STRING_LIMIT || artistDesc.length > STRING_LIMIT) {
-        alert("Input cannot be longer than 40 characters")
-        return;
-    }
-
-    const artistNameSpan = document.createElement("span")
-    artistNameSpan.className = "result_list_item_title"
-    artistNameSpan.textContent = artistName
-
-    const artistDescSpan = document.createElement("span")
-    artistDescSpan.className = "result_list_item_subtitle"
-    artistDescSpan.textContent = artistDesc
-
-    const artistImgElement = document.createElement("img")
-    artistImgElement.src = artistImg
-
-    const artistDetailsContainer = document.createElement("div")
-    artistDetailsContainer.className = "result_list_item_desc"
-    artistDetailsContainer.appendChild(artistNameSpan)
-    artistDetailsContainer.appendChild(artistDescSpan)
-
-    const artistImgContainer = document.createElement("div")
-    artistImgContainer.className = "result_list_item_thumbnail"
-    artistImgContainer.appendChild(artistImgElement);
-
-    const deleteBtnContainer = document.createElement("div")
-    deleteBtnContainer.className = "delete_button_container"
-    const deleteBtn = document.createElement("input")
-    deleteBtn.value = "Delete"
-    deleteBtn.type = "button"
-    deleteBtn.className = "delete_button"
-    deleteBtn.onclick = () => removeArtist(newId)
-    deleteBtnContainer.appendChild(deleteBtn)
-
-    const newItem = document.createElement("div")
-    newItem.className = "result_list_item"
-    newItem.id = newId
-    newItem.appendChild(artistImgContainer)
-    newItem.appendChild(artistDetailsContainer)
-    newItem.appendChild(deleteBtnContainer)
-
-    const resultList = document.querySelector("div.result_list");
-    resultList.prepend(newItem);
-
-    artistForm.reset()
-}
-
-const removeArtist = targetId => removeArtistFromFile(targetId)
-    .then(() => {
-        const targetItem = document.getElementById(`${targetId}`)
-
-        const resultList = document.querySelector("div.result_list")
-        resultList.removeChild(targetItem)
-    })
-
-const removeArtistFromFile = id => fetch(`/deleteArtist/${id}`, {
+const removeArtist = id => fetch(`/deleteArtist/${id}`, {
     method: 'DELETE'
+}).then(() => {
+    const targetItem = document.getElementById(`${id}`)
+
+    const resultList = document.querySelector("div.result_list")
+    resultList.removeChild(targetItem)
 })
 
 const search = () => {
--- server/index.js
+++ server/index.js
@@ -1,90 +1,113 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const expressHbs = require('express-handlebars')
-const app = express()
+const bodyParser = require("body-parser");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const expressHbs = require("express-handlebars");
+const app = express();
 
-const FILE_NAME = "Artists.json"
-const PORT = process.env.PORT || 3000
+const FILE_NAME = "Artists.json";
+const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.engine(
-    'hbs',
-    expressHbs({
-        layoutsDir: 'public/views/layouts/',
-        defaultLayout: 'main-layout',
-        extname: 'hbs'
-    })
-)
+  "hbs",
+  expressHbs({
+    layoutsDir: "public/views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs"
+  })
+);
 
-app.set('view engine', 'hbs')
-app.set('views', 'public/views')
+app.set("view engine", "hbs");
+app.set("views", "public/views");
 
-const getAllArtistsFromFile = () => new Promise((resolve, reject) => {
+const getAllArtistsFromFile = () =>
+  new Promise((resolve, reject) => {
     fs.readFile(FILE_NAME, (err, rawData) => {
-        if (err) reject(err)
-        else resolve(rawData)
-    })
-})
-    .then(rawData => Object.assign({}, rawData && JSON.parse(rawData)))
+      if (err) reject(err);
+      else resolve(rawData);
+    });
+  }).then(rawData => Object.assign({}, rawData && JSON.parse(rawData)));
 
-const writeArtistsToFile = artists => new Promise((resolve, reject) => {
+const writeArtistsToFile = artists =>
+  new Promise((resolve, reject) => {
     fs.writeFile(FILE_NAME, JSON.stringify(artists), (err, data) => {
-        if (err) reject(err)
-        else resolve(artists)
-    })
-})
+      if (err) reject(err);
+      else resolve(artists);
+    });
+  });
 
-app.get('/', (req, res) => res.render('home', {}))
+app.get("/", (req, res) =>
+  getAllArtistsFromFile()
+    .then(data =>
+      res.render("home", {
+        artists: Object.keys(data).map(k =>
+          Object.assign({}, data[k], { id: k })
+        )
+      })
+    )
+    .catch(() => res.status(400))
+);
 
-app.get('/getAllArtists', (req, res) => getAllArtistsFromFile()
-    .then(data => res.render('home', { artists: Object.keys(data).map(k => Object.assign({}, data[k], { id: k })) }))
-    .catch(() => res.status(400)))
-
-app.post('/addNewArtist', (req, res) => getAllArtistsFromFile()
+app.post("/addNewArtist", (req, res) =>
+  getAllArtistsFromFile()
     .then(result => {
-        const newArtist = req.body
-        const finalResult = Object.assign({}, result, newArtist)
-        return writeArtistsToFile(finalResult)
-            // .then(data => res.json(data))
-            .then(data => res.render('home', { artists: Object.keys(data).map(k => Object.assign({}, data[k], { id: k })) }))
-            .catch(err => { throw err })
+      const newArtist = req.body;
+
+      const newId = new Date().getTime();
+      const formedArtist = {};
+      formedArtist[`${newId}`] = newArtist;
+
+      const finalResult = Object.assign({}, result, formedArtist);
+      return writeArtistsToFile(finalResult)
+        .then(() => res.redirect("/"))
+        .catch(err => {
+          throw err;
+        });
     })
-    .catch(() => res.status(400)))
+    .catch(() => res.status(400))
+);
 
-app.delete('/deleteArtist/:id', (req, res) => getAllArtistsFromFile()
+app.delete("/deleteArtist/:id", (req, res) =>
+  getAllArtistsFromFile()
     .then(artistList => {
-        const artistId = req.params.id
+      const artistId = req.params.id;
 
-        delete artistList[artistId]
-        return writeArtistsToFile(artistList)
-            .then(data => res.json(data))
-            .catch(err => { throw err })
+      delete artistList[artistId];
+      return writeArtistsToFile(artistList)
+        .then(() => res.redirect(200, "/"))
+        .catch(err => {
+          throw err;
+        });
     })
-    .catch(() => res.status(400)))
+    .catch(() => res.status(400))
+);
 
-app.get('/getArtistByName/:name', (req, res) => getAllArtistsFromFile()
+app.get("/getArtistByName/:name", (req, res) =>
+  getAllArtistsFromFile()
     .then(artistList => {
-        const targetName = req.params.name
+      const targetName = req.params.name;
 
-        const result = Object.keys(artistList).reduce((acc, key) => {
-            const data = artistList[key]
-            if (data.artistName.toLowerCase().includes(targetName.toLowerCase())) {
-                const tempResult = {}
-                tempResult[`${key}`] = data
-                return { ...acc, ...tempResult }
-            } else {
-                return acc
-            }
-        }, {})
-        return res.json(result)
+      const result = Object.keys(artistList).reduce((acc, key) => {
+        const data = artistList[key];
+        if (data.artistName.toLowerCase().includes(targetName.toLowerCase())) {
+          const tempResult = {};
+          tempResult[`${key}`] = data;
+          return { ...acc, ...tempResult };
+        } else {
+          return acc;
+        }
+      }, {});
+    //   return res.json(result);
+    res.render('home', { artists:  Object.keys(result).map(k =>
+        Object.assign({}, result[k], { id: k })
+      )})
     })
     .catch(err => res.status(400))
-)
+);
 
-app.listen(PORT, () => console.log(`Server ready on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}`));
