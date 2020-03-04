a92bcc42e75d2c08aee1dc1092a7489bfe99c226
Henry Ho <henrychho89@gmail.com>
2020-03-03 21:11:47 -0800

    Added process env variables

--- server/util/database.js
+++ server/util/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "comp4711",
+  host: process.env.HOST || "localhost",
+  user: process.env.USER || "root",
+  database: process.env.DATABASE || "comp4711",
   port: 3306,
-  password: "password"
+  password: process.env.PASSWORD || "password"
 });
 
 module.exports = pool.promise();
